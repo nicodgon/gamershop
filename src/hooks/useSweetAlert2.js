789f1127ac38ee59a23af60e205558827ab6807b
nico <nicolasdg19@hotmail.com>
2024-02-16 11:48:54 -0300

    navigate

--- src/hooks/useSweetAlert2.js
+++ src/hooks/useSweetAlert2.js
@@ -1,7 +1,9 @@
 import logo from "../assets/images/icons/logo.png";
 import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 export const useSweetAlert2 = () => {
+  let navigate = useNavigate();
   const finalizePurchase = () => {
     Swal.fire({
       imageUrl: logo,
@@ -12,9 +14,10 @@ export const useSweetAlert2 = () => {
       icon: "success",
       allowOutsideClick: false,
       showConfirmButton: false,
+      timer: 4000,
     });
     setTimeout(() => {
-      window.location.reload();
+      navigate('/');
     }, 5000);
   };
   return { finalizePurchase };
